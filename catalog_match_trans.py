8edf5f303113912a702444ea97fc385de6977a49
Gregory Rudnick <grudnick@ku.edu>
2018-01-12 12:32:21 -0600

    correct geomap input

--- catalog_match_trans.py
+++ catalog_match_trans.py
@@ -3,6 +3,7 @@ from astropy import units as u
 from astropy.coordinates import SkyCoord
 import matplotlib.pyplot as plt
 from matplotlib import rc
+from matplotlib.ticker import FormatStrFormatter
    
 
 def cat_sky_match(raref, decref, rain, decin, septol, **kwargs):
@@ -55,26 +56,28 @@ def cat_sky_match(raref, decref, rain, decin, septol, **kwargs):
     iclose=iclose[0]
 
     #print(d2d[indx])
-    print(np.column_stack((raref[iclose],rain[idx[iclose]],d2d[iclose]/u.deg*3600.)))
+    #print(np.column_stack((raref[iclose],rain[idx[iclose]],d2d[iclose]/u.deg*3600.)))
 
 
     #write files
     keys = sorted(kwargs.keys())
     for kw in keys:
         if kw == 'matchfile':
-            print(kwargs[kw])
+            #print(kwargs[kw])
             #open file for writing and write a header
             fo = open(kwargs[kw], "w")
             fo.write("# raref decref rain decin\n")
             for i,val in enumerate(iclose):
                 #print(i,iclose[i])
-                print(raref[iclose[i]],decref[iclose[i]],rain[idx[iclose[i]]],decin[idx[iclose[i]]])
+                #print(raref[iclose[i]],decref[iclose[i]],rain[idx[iclose[i]]],decin[idx[iclose[i]]])
                 fo.write('{} {} {} {}\n'.format(raref[iclose[i]],decref[iclose[i]],rain[idx[iclose[i]]],decin[idx[iclose[i]]]))
             fo.close()
 
+    #store the limits of the coordinates
+    lims = {'ramax' : np.amax(raref[iclose]), 'ramin' : np.amin(raref[iclose]), 'decmax' : np.amax(decref[iclose]), 'decmin' : np.amin(decref[iclose])}
     
     #return all matches within the tolerance
-    return raref[iclose], decref[iclose], rain[idx[iclose]], decin[idx[iclose]]
+    return raref[iclose], decref[iclose], rain[idx[iclose]], decin[idx[iclose]], lims
 
 
 
@@ -88,6 +91,10 @@ def match_diff_plot(rarefm, decrefm, rainm, decinm, **kwargs):
     rarefm, decrefm, rainm, decinm: A set of coordinates for matched
     objects.  These arrays must all be the same length.
 
+    OPTIONAL KEWORD PARAMETERS
+
+    plotfile: the name of the file containing the plot
+
     OUTPUT:
 
     a plot of the differences between the RA and DEC coordinates
@@ -98,18 +105,28 @@ def match_diff_plot(rarefm, decrefm, rainm, decinm, **kwargs):
     rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
     rc('text', usetex=True)
 
-    plt.clf()
     
+    lims = {'ramax' : np.amax(rarefm), 'ramin' : np.amin(rarefm), 'decmax' : np.amax(decrefm), 'decmin' : np.amin(decrefm)}
+    print("plotlims are ",lims)
+
+    padfac = 0.001
     radiff = (rarefm - rainm)*3600.
     decdiff = (decrefm - decinm)*3600.
     #f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex='col', sharey='row')
-    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-    ax1.scatter(rarefm, radiff)
-    ralims = [8.85, 9.1]
-    declims = [-43.29, -43.12]
+    ralims = [lims['ramin'] * (1. - padfac), lims['ramax'] * (1. + padfac)]
+
+    #handles how to do padding if DECs are positive or negative
+    if lims['decmin'] < 0:
+        declims = [lims['decmin'] * (1. + padfac/10.), lims['decmax'] * (1. - padfac/10.)]
+    else:
+        declims = [lims['decmin'] * (1. - padfac/10.), lims['decmax'] * (1. + padfac/10.)]
     yline = [0,0]
     yline1 = [-0.5, -0.5]
     yline2 = [0.5, 0.5]
+
+    #plt.clf()
+    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+    ax1.scatter(rarefm, radiff)
     ax1.plot(ralims, yline, color='r')
     ax1.plot(ralims, yline1, color='r', linestyle = ':')
     ax1.plot(ralims, yline2, color='r', linestyle = ':')
@@ -121,7 +138,8 @@ def match_diff_plot(rarefm, decrefm, rainm, decinm, **kwargs):
     ax2.plot(declims, yline, color='r')
     ax2.plot(declims, yline1, color='r', linestyle = ':')
     ax2.plot(declims, yline2, color='r', linestyle = ':')
-    ax2.set_xticks(np.arange(min(decrefm), max(decrefm)+0.04, 0.04))
+    #ax2.set_xticks(np.arange(min(decrefm), max(decrefm)+0.04, 0.04))
+    ax2.xaxis.set_major_formatter(FormatStrFormatter('%.3f'))
     ax2.set_xlim(declims)
     ax2.set_ylim([-3.,3.])
 
@@ -138,7 +156,8 @@ def match_diff_plot(rarefm, decrefm, rainm, decinm, **kwargs):
     ax4.plot(declims, yline, color='r')
     ax4.plot(declims, yline1, color='r', linestyle = ':')
     ax4.plot(declims, yline2, color='r', linestyle = ':')
-    ax4.set_xticks(np.arange(min(decrefm), max(decrefm)+0.04, 0.04))
+    #ax4.set_xticks(np.arange(min(decrefm), max(decrefm)+0.04, 0.04))
+    ax4.xaxis.set_major_formatter(FormatStrFormatter('%.3f'))
     ax4.set_xlim(declims)
     ax4.set_ylim([-3.,3.])
     ax4.set_xlabel(r'Dec')
